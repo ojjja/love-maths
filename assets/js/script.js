7575775817d2c6f4a979e04a38875c827821a6e4
ojjja <oyvind@skidspelen.se>
2022-10-06 19:22:46 +0000

    add eventlistner for document loading and buttons clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//code to be executed when the page has finnish loading_Wait for the dom finnish loading before running the game
+//code to be executed when the user clicks a button_Get the button elements and add eventslistner to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit");
+            }else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked $(gameType)`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
