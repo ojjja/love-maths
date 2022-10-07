e240c1cbf9b5136c6d6203847bb36802c2d35c73
ojjja <oyvind@skidspelen.se>
2022-10-07 09:14:11 +0000

    fix error in alert for game-type

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit");
             }else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked $(gameType)`);
+                alert(`You clicked ${gameType}`);
             }
         })
     }
