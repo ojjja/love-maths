2a18f8a1651b86636493c5aa16c05af3a00b0677
ojjja <oyvind@skidspelen.se>
2022-10-07 12:57:07 +0000

    start calculating correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,21 @@ function checkAnswer() {
 
 }
 
+/**
+ * gets the operands(the numbers) and the operator (plus, minus, etc)
+ * directly from the DOM and returns the direct answer
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === "+") {
+        return [operator1 + operator2, "addition"]
+    }else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
 
